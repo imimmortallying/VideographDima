3f86589087cb3a1dd1c86b75329fd8ca0ab14403
Alexey Bylina <imimmortallyingwork@yandex.ru>
2024-06-22 12:59:39 +0700

    manual vendor prefix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// import {browserslistToTargets} from 'lightningcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,9 @@ export default defineConfig({
         `,
       },
     },
+    // transformer: 'lightningcss'
   },
+  build: {
+    cssMinify: 'lightningcss'
+  }
 });
