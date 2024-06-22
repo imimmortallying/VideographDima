ea37f03645445b609b83e5921cd872a102fb06c3
Alexey Bylina <imimmortallyingwork@yandex.ru>
2024-06-22 11:16:21 +0700

    phone-small breakpoint added

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -68,8 +68,16 @@ function Header({ showModal }: Header) {
               title: "мы создаем уникальный видеоконтент",
               markedWord: "уникальный",
             },
-            { title: "видеосъемка для вашего бизнеса", markedWord: "бизнеса" },
-            { title: "видеопродакшен полного цикла", markedWord: "полного" },
+            {
+              title: "мы создаем уникальный видеоконтент",
+              markedWord: "уникальный",
+            },
+            {
+              title: "мы создаем уникальный видеоконтент",
+              markedWord: "уникальный",
+            },
+            // { title: "видеосъемка для вашего бизнеса", markedWord: "бизнеса" },
+            // { title: "видеосъемка полного цикла", markedWord: "полного" },
           ]}
           className={cls.header_fader}
           markedWordClassName={cls.header_marked}
